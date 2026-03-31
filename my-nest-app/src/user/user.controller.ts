8664760b256dbb70a7cb7bac2207c62db3c70e09
aushamim <au.shamim0@gmail.com>
2026-03-31 11:28:16 +0600

    first commit

---
+++ my-nest-app/src/user/user.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getUser(): string {
+    return 'This is the user controller';
+  }
+
+  @Get('all')
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.userService.getUserById(parseInt(id));
+  }
+}
---
+++ my-nest-app/src/user/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+  private users: { id: number; name: string }[] = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' },
+  ];
+
+  getAllUsers() {
+    return this.users;
+  }
+
+  getUserById(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+}
