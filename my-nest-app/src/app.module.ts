0d64b9f83efd59ac1c7fb8554b4650f92413010e
aushamim <au.shamim0@gmail.com>
2026-04-06 16:15:51 +0600

    middleware

--- my-nest-app/src/app.module.ts
+++ my-nest-app/src/app.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger/logger.middleware';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
@@ -8,4 +9,8 @@ import { UserModule } from './modules/user/user.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
